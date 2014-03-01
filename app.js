a6f11e8be842bf9aa610b1c77d99039e5f16216f
david viramontes <dviramontes@gmail.com>
2014-03-01 14:52:22 -0700

    styles before scripts

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+'use strict';
+
 var request = require('request');
 var restify = require('restify');
-var chalk = require('chalk');
+// var chalk = require('chalk');
 var _ = require('lodash');
 
 var server = restify.createServer({
-  name: 'adoptDenver',
-  version: '0.0.1'
+    name: 'adoptDenver',
+    version: '0.0.1'
 });
 
 var regex = {
-  'one': new RegExp(/ID=([^&]+)/gi), // id
-  'two': new RegExp(/<font\ class="Title">*([^&]+)/gi), // name 
+    'one': new RegExp(/ID=([^&]+)/gi), // id
+    'two': new RegExp(/<font\ class='Title'>*([^&]+)/gi), // name 
 };
 
-var name, desc, _id;
-
-var _id = undefined;
+var _id;
 var idParams = [];
 
 var endpoint = 'http://www.petharbor.com/petoftheday.asp?shelterlist=%27DNVR%27&imgwid=160&imght=120&imgname=POD&bgcolor=FFFFFF&fgcolor=000000&type=dog&border=0&availableonly=1&SEQ=0&SHOWSTAT=1&fontface=arial&fontsize=2&noclientinfo=0&bigtitle=1&source=results';
@@ -34,76 +34,81 @@ restify.CORS.ALLOW_HEADERS.push('withcredentials');
 restify.CORS.ALLOW_HEADERS.push('x-requested-with');
 server.use(restify.CORS());
 
-server.get('/', function (req, res, next) {
+server.get(/\/public\/?.*/, restify.serveStatic({
+    directory: './public'
+}));
 
-  request(endpoint, function (err, req, body) {
-    errorHandler(err);
-    var _body = body;
-    idParams = regex.one.exec(_body);
-    _id = idParams[1];
-    var stringRegExp = "[^" + _id + "<BR><BR><\/font>][^<BR><br>]"
-    var regexpSix = new RegExp(stringRegExp, "g");
+server.get('/api/', function(req, res) {
+    request(endpoint, function(err, req, body) {
+        errorHandler(err);
+        var _body = body,
+            name;
+        idParams = regex.one.exec(_body);
+        _id = idParams[1];
+        // var stringRegExp = '[^' + _id + '<BR><BR><\/font>][^<BR><br>]';
+        // var regexpSix = new RegExp(stringRegExp, 'g');
 
-    var endpointTwo = 'http://www.petharbor.com/detail.asp?ID=' + _id + '&LOCATION=DNVR&searchtype=rnd&shelterlist=%27DNVR%27&where=dummy&kiosk=1';
+        var endpointTwo = 'http://www.petharbor.com/detail.asp?ID=' + _id + '&LOCATION=DNVR&searchtype=rnd&shelterlist=%27DNVR%27&where=dummy&kiosk=1';
 
-    request(endpointTwo, function (err, req, body) {
-      errorHandler(err);
-      var _body = body;
-      var nameParams = regex.two.exec(_body);
+        request(endpointTwo, function(err, req, body) {
+            errorHandler(err);
+            var _body = body,
+                desc;
+            var nameParams = regex.two.exec(_body);
 
-      try {
-        if (_.isNull(nameParams)) {
-          // return next();
-          res.send(500, "error in parsing first response");
-        } else {
-          var name = nameParams[1];
-        }
+            try {
+                if (_.isNull(nameParams)) {
+                    // return next();
+                    res.send(500, 'error in parsing first response');
+                } else {
+                    name = nameParams[1];
+                }
 
-      } catch (e) {
-        console.log(e);
-        throw e;
-      }
+            } catch (e) {
+                console.log(e);
+                throw e;
+            }
 
-      var stringRegExp = _id + "<BR><BR><\/font>(.*?)<BR><br>";
-      var regexpSeven = new RegExp(stringRegExp, "g");
-      var responseBody = regexpSeven.exec(_body);
+            var stringRegExp = _id + '<BR><BR><\/font>(.*?)<BR><br>';
+            var regexpSeven = new RegExp(stringRegExp, 'g');
+            var responseBody = regexpSeven.exec(_body);
 
-      try {
-        if (_.isNull(responseBody)) {
-          console.log(responseBody)
-          res.end("error in parsing second response");
-        } else {
-          var desc = responseBody[1];
-        }
-      } catch (e) {
-        console.log(e);
-        throw e;
-      }
+            try {
+                if (_.isNull(responseBody)) {
+                    console.log(responseBody)
+                    res.end('error in parsing second response');
+                } else {
+                    desc = responseBody[1];
+                }
+            } catch (e) {
+                console.log(e);
+                throw e;
+            }
 
-      desc = desc.replace(/<BR><BR>/, " ").replace(/<BR><BR>/, " ");
-      // payload for request
-      var animaldata = {
-        name: name,
-        pic: "http://www.petharbor.com/get_image.asp?RES=detail&ID=" + _id + "&LOCATION=DNVR",
-        id: _id,
-        desc: desc
-      };
+            desc = desc.replace(/<BR><BR>/, ' ').replace(/<BR><BR>/, ' ');
+            // payload for request
+            var animaldata = {
+                name: name,
+                pic: 'http://www.petharbor.com/get_image.asp?RES=detail&ID=' + _id + '&LOCATION=DNVR',
+                id: _id,
+                desc: desc
+            };
 
-      res.send(animaldata);
-      // res.end();
+            res.send(animaldata);
+            // res.end();
+        });
     });
-  });
 });
 
-var errorHandler = function (e) {
-  if (e) {
-    console.error(e);
-    next(e);
-  }
-  // if (e) return next(new restify.InvalidArgumentError(JSON.stringify(e.errors)));
-}
+var errorHandler = function(e, next) {
+    if (e) {
+        console.error(e);
+        next(e);
+    }
+    // if (e) return next(new restify.InvalidArgumentError(JSON.stringify(e.errors)));
+};
 
 var port = process.env.PORT || 8080;
-server.listen(port, function () {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(port, function() {
+    console.log('%s listening at %s', server.name, server.url);
 })
--- public/load-random-animal.js
+++ public/load-random-animal.js
@@ -1,19 +1,16 @@
-
 //requires jquery
-$(function  () {
-	var animaldata;
+$(function() {
+    var animaldata;
 
-	$.getJSON("http://adoptabledenver.herokuapp.com/", function(data) {
-    animaldata = data;
-    // data is a JavaScript object now. Handle it as such
-   //Get the HTML from the template   in the script tag
-    var theTemplateScript = $("#animal-template").html(); 
+    $.getJSON("http://adoptabledenver.herokuapp.com/", function(data) {
+        animaldata = data;
+        // data is a JavaScript object now. Handle it as such
+        //Get the HTML from the template   in the script tag
+        var theTemplateScript = $("#animal-template").html();
 
-   //Compile the template
-    var compiledTemplate = Handlebars.compile (theTemplateScript); 
-    $('#the-thing').append (compiledTemplate (animaldata));
-
-	});
-	
+        //Compile the template
+        var compiledTemplate = Handlebars.compile(theTemplateScript);
+        $('#the-thing').append(compiledTemplate(animaldata));
+    });
 
 });
